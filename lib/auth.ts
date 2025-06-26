6606bf50cae17913248d734d8995d9e912fe129c
ajikia15 <aleksandrej15@gmail.com>
2025-06-26 21:57:01 +0400

    feat(auth): add Google sign-in functionality and update auth configuration for social providers

--- lib/auth.ts
+++ lib/auth.ts
@@ -8,12 +8,12 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  // socialProviders: {
-  //     github: {
-  //        clientId: process.env.GITHUB_CLIENT_ID as string,
-  //        clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-  //     },
-  // },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   database: drizzleAdapter(db, {
     provider: "sqlite",
     schema,
