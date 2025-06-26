50eb2f7a2711b1cbb4dd2dce5316b92b8c14d60b
ajikia15 <aleksandrej15@gmail.com>
2025-06-26 22:01:33 +0400

    feat(profile): create profile page component feat(signup): implement signup page with form integration and Google sign-in feat(signup-form): add signup form with validation and submission handling

---
+++ app/signup/page.tsx
@@ -0,0 +1,12 @@
+import { SignupForm } from "@/components/signup-form";
+import Logo from "../components/Logo";
+export default function SignupPage() {
+  return (
+    <div className="bg-muted min-h-svh flex flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Logo />
+        <SignupForm />
+      </div>
+    </div>
+  );
+}
